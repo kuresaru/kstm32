447a2a9a31c1e0ae233e28592af449747eced899
暮光小猿wzt(PC) <1142305747@qq.com>
2019-10-07 21:33:33 +0800

    Include从新配置读取

--- src/config.ts
+++ src/config.ts
@@ -12,43 +12,65 @@ export function getWorkspaceRoot(): vscode.Uri | undefined {
     }
 }
 
-export function addInclude(include: string): boolean {
+function getConfig(): any | undefined {
     let uri: vscode.Uri | undefined = getWorkspaceRoot();
     if (uri) {
         uri = vscode.Uri.parse(uri + CONFIG_FILENAME);
-        let buffer = fs.readFileSync(uri.fsPath, {encoding: 'UTF-8'});
+        let buffer = fs.readFileSync(uri.fsPath, { encoding: 'UTF-8' });
         let json = JSON.parse(buffer.toString());
-        if (!json.includes) {
-            json.includes = [];
+        return json;
+    }
+}
+
+function saveConfig(config: any) {
+    let uri: vscode.Uri | undefined = getWorkspaceRoot();
+    if (uri) {
+        uri = vscode.Uri.parse(uri + CONFIG_FILENAME);
+        fs.writeFileSync(uri.fsPath, JSON.stringify(config), { encoding: 'UTF-8' });
+    }
+}
+
+export function addInclude(include: string): boolean {
+    let config = getConfig();
+    if (config) {
+        if (!config.includes) {
+            config.includes = [];
         }
-        for (let i in json.includes) {
-            if (json.includes[i] == include) {
+        for (let i in config.includes) {
+            if (config.includes[i] == include) {
                 return true;
             }
         }
-        json.includes.push(include);
-        fs.writeFileSync(uri.fsPath, JSON.stringify(json), {encoding: 'UTF-8'});
+        config.includes.push(include);
+        saveConfig(config);
         return true;
     }
     return false;
 }
 
 export function rmInclude(include: string): boolean {
-    let uri: vscode.Uri | undefined = getWorkspaceRoot();
-    if (uri) {
-        uri = vscode.Uri.parse(uri + CONFIG_FILENAME);
-        let buffer = fs.readFileSync(uri.fsPath, {encoding: 'UTF-8'});
-        let json = JSON.parse(buffer.toString());
-        if (!json.includes) {
-            json.includes = [];
+    let config = getConfig();
+    if (config) {
+        if (!config.includes) {
+            return true;
         }
-        for (let i in json.includes) {
-            if (json.includes[i] == include) {
-                json.includes.splice(i, 1);
+        for (let i in config.includes) {
+            if (config.includes[i] == include) {
+                config.includes.splice(i, 1);
             }
         }
-        fs.writeFileSync(uri.fsPath, JSON.stringify(json), {encoding: 'UTF-8'});
+        saveConfig(config);
         return true;
     }
     return false;
+}
+
+export function getInclude(): string[] {
+    let config = getConfig();
+    if (config) {
+        if (config.includes) {
+            return config.includes;
+        }
+    }
+    return [];
 }
--- src/treeProviders/tpCIncludes.ts
+++ src/treeProviders/tpCIncludes.ts
@@ -1,16 +1,13 @@
 import * as vscode from 'vscode';
 import * as tpTemplate from './tpTemplate';
+import * as config from '../config'
 
 export class TPCIncludes extends tpTemplate.tpTemplate<vscode.TreeItem>{
     getChildren(element?: vscode.TreeItem) {
         let result: vscode.TreeItem[] = [];
         //root
         if (!element) {
-            let cSources: string[] | undefined = vscode.workspace.getConfiguration('kstm32').get('cincludes');
-            if (!cSources) {
-                cSources = [];
-            }
-            cSources.forEach(element => result.push(new vscode.TreeItem(element)));
+            config.getInclude().forEach(element => result.push(new vscode.TreeItem(element)));
         }
         return Promise.resolve(result);
     }
